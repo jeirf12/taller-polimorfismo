bfbc6f813cabf4d6634d110c6d9f592810a19d80
fallen <jhonfer.ruiz@gmail.com>
2021-02-17 13:47:20 -0500

    project clear

--- src/main/java/co/unicauca/tallerpolimorfismo/cliente/ClienteMain.java
+++ src/main/java/co/unicauca/tallerpolimorfismo/cliente/ClienteMain.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.unicauca.tallerpolimorfismo.cliente;
 
 import co.unicauca.tallerpolimorfismo.modelo.*;
@@ -14,7 +9,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author fallen
+ * @author Jhonfer Ruiz
  */
 public class ClienteMain {
 
--- src/test/java/co/unicauca/tallerpolimorfismo/modelo/ViajeIT.java
+++ src/test/java/co/unicauca/tallerpolimorfismo/modelo/ViajeIT.java
@@ -1,44 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.unicauca.tallerpolimorfismo.modelo;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author fallen
+ * @author Jhonfer Ruiz
  */
 public class ViajeIT {
     
     public ViajeIT() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    
+    }    
     /**
      * Test de la clase ViajeFamiliar.
      *
